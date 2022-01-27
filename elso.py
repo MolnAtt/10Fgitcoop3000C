8ca490db183f3d257f4019a173a5279c0b34d677
MolnAtt <molnar.attila@szlgbp.hu>
2022-01-27 13:48:48 +0100

    kezdjünkelbrencselni

---
+++ elso.py
@@ -0,0 +1,9 @@
+# 1. Van-e a sorozatban 100-zal osztható szám?
+
+def hello_vilag():
+    print("hello világ!")
+
+"""
+hello_vilag()
+
+"""
--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 print("haliho")
 
+from elso import hello_vilag
+
+lista = []
+with open('input.txt', "r", encoding="utf8") as f:
+    for sor in f:
+        lista.append(sor)
+
+hello_vilag()
